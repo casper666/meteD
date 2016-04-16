053517ad63512bf6f625e07abda5b36516ebc213
casper666 <lbjyy@hotmail.com>
2016-04-16 18:55:48 -0400

    final app side

--- app/src/main/java/edu/dartmouth/cs/meted/MapsActivity.java
+++ app/src/main/java/edu/dartmouth/cs/meted/MapsActivity.java
@@ -2,8 +2,8 @@ package edu.dartmouth.cs.meted;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.location.Location;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
@@ -11,10 +11,18 @@ import com.google.android.gms.location.LocationListener;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -33,12 +41,26 @@ import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
 import com.google.maps.android.clustering.algo.GridBasedAlgorithm;
 import com.google.maps.android.clustering.algo.PreCachingAlgorithmDecorator;
+import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+import com.google.maps.android.ui.IconGenerator;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -54,18 +76,75 @@ public class MapsActivity extends AppCompatActivity implements GoogleApiClient.C
     private ClusterManager<ParkSlot> mClusterManager;
     // Declare floating button
     private FloatingActionButton mButton;
+    // server address
+//    public static final String SERVER_ADDR = "http://169.254.180.233:8080";
+    public static final String SERVER_ADDR = "https://data-infinity-128221.appspot.com";
+    ArrayList<ParkSlot> mList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         mButton = (FloatingActionButton) findViewById(R.id.fab);
-
+        // set up toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // gcm register
         new GcmRegistrationAsyncTask(this).execute();
+        // test data
+//        mList.add(new ParkSlot(43.704393, -72.287888, new Long(System.currentTimeMillis()/1000 + 100)));
+//        mList.add(new ParkSlot(43.704447, -72.287891, new Long(System.currentTimeMillis()/1000 + 100)));
+//        mList.add(new ParkSlot(43.704529, -72.287894, new Long(System.currentTimeMillis()/1000 + 100)));
+//        mList.add(new ParkSlot(43.704590, -72.287891, new Long(System.currentTimeMillis()/1000 + 15)));
+//        mList.add(new ParkSlot(43.704738, -72.287885, new Long(System.currentTimeMillis()/1000 + 20)));
+//        mList.add(new ParkSlot(43.704826, -72.287881, new Long(System.currentTimeMillis()/1000 + 100)));
+//        mList.add(new ParkSlot(43.704897, -72.287875, new Long(System.currentTimeMillis()/1000 + 100)));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Snackbar.make(findViewById(R.id.layout), " Happy HackDartmouth 2016!\n Developed by Jason, Noel, Jay & Wayne", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show();
+        downloadTask task = new downloadTask();
+        startAnim();
+        task.execute();
+    }
+
+    void startAnim(){
+        findViewById(R.id.avloadingIndicatorView).setVisibility(View.VISIBLE);
+    }
+
+    void stopAnim(){
+        findViewById(R.id.avloadingIndicatorView).setVisibility(View.GONE);
     }
 
     /**
@@ -80,6 +159,7 @@ public class MapsActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+//        Toast.makeText(this,"onMapReady",Toast.LENGTH_SHORT).show();
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -102,10 +182,20 @@ public class MapsActivity extends AppCompatActivity implements GoogleApiClient.C
                 mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
+
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        ScheduledFuture scheduledFuture = scheduler.scheduleAtFixedRate(new Thread() {
+
+            @Override
+            public void run() {
+                downloadTask dt = new downloadTask();
+                dt.execute();
+            }
+        }, 0, 10, TimeUnit.SECONDS);
     }
 
     protected synchronized void buildGoogleApiClient() {
-        Toast.makeText(this, "buildGoogleApiClient", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "buildGoogleApiClient", Toast.LENGTH_SHORT).show();
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -115,7 +205,7 @@ public class MapsActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(Bundle bundle) {
-        Toast.makeText(this, "onConnected", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "onConnected", Toast.LENGTH_SHORT).show();
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -165,6 +255,36 @@ public class MapsActivity extends AppCompatActivity implements GoogleApiClient.C
         //LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
     }
 
+    /**
+     * Draws profile photos inside markers (using IconGenerator).
+     * When there are multiple people in the cluster, draw multiple photos (using MultiDrawable).
+     */
+    private class SlotRenderer extends DefaultClusterRenderer<ParkSlot> {
+        private final IconGenerator mIconGenerator = new IconGenerator(getApplicationContext());
+        private final ImageView mImageView;
+
+        public SlotRenderer() {
+            super(getApplicationContext(), mMap, mClusterManager);
+            mImageView = new ImageView(getApplicationContext());
+            mImageView.setLayoutParams(new ViewGroup.LayoutParams(36, 36));
+            mIconGenerator.setContentView(mImageView);
+        }
+
+        @Override
+        protected void onBeforeClusterItemRendered(ParkSlot slot, MarkerOptions markerOptions) {
+            // Draw a single slot.
+            mImageView.setImageResource(R.drawable.parking);
+            Bitmap icon = mIconGenerator.makeIcon();
+            markerOptions.icon(BitmapDescriptorFactory.fromBitmap(icon)).title("Available Now! Price: 0.25$ per quarter");
+        }
+
+        @Override
+        protected boolean shouldRenderAsCluster(Cluster cluster) {
+            // Always render clusters.
+            return cluster.getSize() > 1;
+        }
+    }
+
     private void setUpClusterer() {
 
         // Initialize the manager with the context and the map.
@@ -177,66 +297,102 @@ public class MapsActivity extends AppCompatActivity implements GoogleApiClient.C
         mMap.setOnMarkerClickListener(mClusterManager);
         // mClusterManager.setAlgorithm(new PreCachingAlgorithmDecorator<ParkSlot>(new GridBasedAlgorithm<ParkSlot>()));
         mClusterManager.setAlgorithm(new CustomNonHierarchicalDistanceBasedAlgorithm<ParkSlot>());
+        mClusterManager.setRenderer(new SlotRenderer());
         // Add cluster items (markers) to the cluster manager.
         addItems();
     }
 
     private void addItems() {
 
-        // Set some lat/lng coordinates to start with.
-        double lat = 43.705294;
-        double lng = -72.287735;
-
-        ParkSlot offsetItem = new ParkSlot(0,0);
-
-        // Add ten cluster items in close proximity, for purposes of this example.
-        for (int i = 0; i < 10; i++) {
-            double offset = i / 30000d;
-            lat = lat + offset;
-            lng = lng + offset;
-            offsetItem = new ParkSlot(lat, lng);
-            mClusterManager.addItem(offsetItem);
+        if(mClusterManager == null) {
+            Toast.makeText(this,"error",Toast.LENGTH_SHORT).show();
+            return;
         }
+        mClusterManager.clearItems();
+        for(ParkSlot ps : mList) {
+            if(isAvailable(ps)){
+                mClusterManager.addItem(ps);
+                Log.d("add", "123123");
+            }
+        }
+        mClusterManager.cluster();
 
-        final ParkSlot removeItem = offsetItem;
-
-//        Thread thread = new Thread(new Runnable() {
+//        /*** test asynctask to remove item ***/
+//        final ParkSlot removeItem = offsetItem;
+//
+//        new AsyncTask<Void, Void, Void>() {
 //
 //            @Override
-//            public void run() {
+//            protected Void doInBackground(Void... voids) {
 //                try {
-//                    Thread.sleep(2000);
+//                    Thread.sleep(5000);
 //                } catch (InterruptedException e) {
 //                    e.printStackTrace();
 //                }
+//                return null;
+//            }
+//
+//            @Override
+//            protected void onPostExecute(Void aVoid) {
+//                super.onPostExecute(aVoid);
 //                mClusterManager.removeItem(removeItem);
 //                mClusterManager.cluster();
 //            }
-//        });
-//
-//        thread.start();
-
-        new AsyncTask<Void, Void, Void>() {
-
-            @Override
-            protected Void doInBackground(Void... voids) {
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                mClusterManager.removeItem(removeItem);
-                mClusterManager.cluster();
-            }
-        }.execute();
+//        }.execute();
 
     }
+
+    private boolean isAvailable(ParkSlot ps) {
+        long cur = System.currentTimeMillis()/1000;
+        Log.d("time",""+cur + " >>>>><<<<< " + ps.mOccupiedEndTime.longValue());
+        return cur > ps.mOccupiedEndTime.longValue();
+    }
+
+    private class downloadTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String returnData = "";
+            Map<String, String> params = new HashMap<>();
+            params.put("data", "=");
+            try {
+                returnData = ServerUtilities.post(SERVER_ADDR + "/querymob.do", params);
+                Thread.sleep(1000);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return returnData;
+        }
+
+        @Override
+        protected void onPostExecute(String strs) {
+            super.onPostExecute(strs);
+            mList.clear();
+            JSONArray spotArry = new JSONArray();
+            try {
+                spotArry = new JSONArray(strs);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i < spotArry.length(); i++) {
+                try {
+                    JSONObject element = spotArry.getJSONObject(i);
+                    double lat = element.getDouble("lat");
+                    double lng = element.getDouble("long");
+                    Long endTime = element.getLong("end");
+                    mList.add(new ParkSlot(lat,lng,endTime));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+//            Toast.makeText(getApplicationContext(),mList.size()+">>>>",Toast.LENGTH_SHORT).show();
+            addItems();
+            stopAnim();
+        }
+    }
+
     static class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
         private static Registration regService = null;
         private GoogleCloudMessaging gcm;
@@ -252,18 +408,20 @@ public class MapsActivity extends AppCompatActivity implements GoogleApiClient.C
         @Override
         protected String doInBackground(Void... params) {
             if (regService == null) {
-                Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        // Need setRootUrl and setGoogleClientRequestInitializer only for local testing,
-                        // otherwise they can be skipped
-                        .setRootUrl("http://169.254.180.233:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
-                                    throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
+//                Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(),
+//                        new AndroidJsonFactory(), null)
+//                        // Need setRootUrl and setGoogleClientRequestInitializer only for local testing,
+//                        // otherwise they can be skipped
+//                        .setRootUrl(SERVER_ADDR + "/_ah/api/")
+//                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+//                            @Override
+//                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
+//                                    throws IOException {
+//                                abstractGoogleClientRequest.setDisableGZipContent(true);
+//                            }
+//                        });
+                Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl(SERVER_ADDR + "/_ah/api/");
                 // end of optional local run code
 
                 regService = builder.build();
@@ -292,7 +450,7 @@ public class MapsActivity extends AppCompatActivity implements GoogleApiClient.C
 
         @Override
         protected void onPostExecute(String msg) {
-            Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+            Toast.makeText(context, "Registered with GAE", Toast.LENGTH_LONG).show();
             Logger.getLogger("REGISTRATION").log(Level.INFO, msg);
         }
     }
--- app/src/main/java/edu/dartmouth/cs/meted/ParkSlot.java
+++ app/src/main/java/edu/dartmouth/cs/meted/ParkSlot.java
@@ -9,8 +9,63 @@ import com.google.maps.android.clustering.ClusterItem;
 public class ParkSlot implements ClusterItem {
     private final LatLng mPosition;
 
-    public ParkSlot(double lat, double lng) {
+    public ParkSlot(double lat, double lng, Long endTime) {
         mPosition = new LatLng(lat, lng);
+        mOccupiedEndTime = endTime;
+    }
+
+    public ParkSlot(double _longitude, double _latitude, String _streetName,
+                    int _dailyFreeParkingStartTime, int _dailyFreeParkingEndTime,
+                    double _hourlyFee, String _freeDays, boolean _isOccupied,
+                    int _occupiedStartTime, Long _occupiedEndTime) {
+        mLongitude = _longitude;
+        mLatitude = _latitude;
+        mPosition = new LatLng(_latitude,_longitude);
+        mStreetName = _streetName;
+        mDailyFreeParkingStartTime = _dailyFreeParkingStartTime;
+        mDailyFreeParkingEndTime = _dailyFreeParkingEndTime;
+        mHourlyFee = _hourlyFee;
+        mFreeDays = _freeDays;
+        mIsOccupied = _isOccupied;
+        mOccupiedStartTime = _occupiedStartTime;
+        mOccupiedEndTime = _occupiedEndTime;
+    }
+
+    public static final String PARKINGSPOT_PARENT_ENTITY_NAME = "ParkingSpotParent";
+    public static final String PARKINGSPOT_PARENT_KEY_NAME = "ParkingSpotParent";
+
+    public static final String PARKINGSPOT_ENTITY_NAME = "ParkingSpot";
+    public static final String FIELD_NAME_LONGITUDE = "longitude";
+    public static final String FIELD_NAME_LATITUDE = "latitude";
+    public static final String FIELD_NAME_STREETNAME = "streetName";
+    public static final String FIELD_NAME_DAILYFREEPARKINGSTARTTIME = "dailyFreeParkingStartTime";
+    public static final String FIELD_NAME_DAILYFREEPARKINGENDTIME = "dailyFreeParkingEndTime";
+    public static final String FIELD_NAME_HOURLYFEE = "hourlyFee";
+    public static final String FIELD_NAME_FREEDAYS = "freeDays";
+    public static final String FIELD_NAME_ISOCCUPIED = "isOccupied";
+    public static final String FIELD_NAME_OCCUPIEDSTARTTIME = "occupiedStartTime";
+    public static final String FIELD_NAME_OCCUPIEDENDTIME = "occupiedEndTime";
+
+    public double mLongitude;
+    public double mLatitude;
+    public String mStreetName;
+    public int mDailyFreeParkingStartTime;
+    public int mDailyFreeParkingEndTime;
+    public double mHourlyFee;
+    public String mFreeDays;
+    public Boolean mIsOccupied;
+    public int mOccupiedStartTime;
+
+    public Long getmOccupiedEndTime() {
+        return mOccupiedEndTime;
+    }
+
+    public Long mOccupiedEndTime;
+
+    public String getCoordInString () {
+        return Double.toString(this.mLongitude)
+                + "+" +
+                Double.toString(this.mLatitude);
     }
 
     @Override
